b17034167b97ea69384bbca44eefb0cc24174deb
klorin <45018060+klorin@users.noreply.github.com>
2020-01-01 00:26:41 +0200

    Update DataCenter.h

--- DataCenter.h
+++ DataCenter.h
@@ -1,19 +1,76 @@
 //
-// Created by Akiva on 2019-12-30.
+// Created by Roy on 30/12/19.
 //
 
 #ifndef DATACENTERSMANAGER2_DATACENTER_H
 #define DATACENTERSMANAGER2_DATACENTER_H
-
-#include "Data Structures/Set.h"
 #include "Data Structures/AVLTree/AVLTree.h"
 #include "Server.h"
+#include "Data Structures/Set.h"
 
-class DataCenter : public Set<Server> {
-private:
-    AVLTree<Server> DCServers;
+class DataCenter { //Roi's version
+public: //DEBUGGING NOTE: Make public to be able to run the test currently in main
+    int id;
+    int servers_num;
+    AVLTree<Server> servers_tree;
+    Set<DataCenter>::SetNode link;
+    enum DataCenterError{SUCCESS = 0, FAILURE = -1, ALLOCATION_ERROR = -2, INVALID_INPUT = -3};
 public:
-    DataCenter &merge(DataCenter<T> &);
+    //ctor
+    explicit DataCenter(int given_id = 0): id(given_id), servers_num(0) {}
+    //
+    void IncrementSize() {servers_num++;}
+    DataCenterError AddServer(int given_id){
+        //check in outer layers that the id is correct
+        //check in outer layer that id isn;t already inside HashTable
+        Server temp_server(given_id);
+        try {
+            servers_tree.insert(temp_server);
+            return SUCCESS;
+        }
+        catch (DataManagerExceptions::Exceptions &e){
+            return FAILURE;
+        }
+    }
+
+    DataCenterError RemoveServer(int ServerID){
+        //check in outer layers that the id is correct
+        Server temp_server(ServerID);
+        try {
+            servers_tree.remove(temp_server);
+            servers_num--;
+            return SUCCESS;
+        }
+        catch (DataManagerExceptions::Exceptions &e){
+            return FAILURE;
+        }
+    }
+
+    DataCenterError SetTraffic(int ServerID, int traffic){
+        //check in traffic and server number are legal
+        //check server in hash table
+        Server temp_server(ServerID, traffic);
+        servers_tree.remove(temp_server);
+        servers_tree.insert(temp_server);
+    }
+
+    DataCenterError SumHighestTrafficServers(int k, int *traffic){
+     //check k> 0
+     //if (k > servers_num) return FAILURE;
+     //int tree_size = servers_tree.getTreeSize();
+     //Server max_server = servers_tree.getMaxNode();
+     //if (k >= tree_size) {
+     //    *traffic = servers_tree.getPartialSum(max_server);
+     //    return SUCCESS;
+     //}
+     //else{
+     //    k = tree_size - k;
+     //    Server kth_server = servers_tree.getKthNode();
+     //    *traffic = servers_tree.getPartialSum(max_server) - servers_tree.getPartialSum(kth_server);
+         return SUCCESS;
+     }
+     //
+
 };
 
 #endif //DATACENTERSMANAGER2_DATACENTER_H
