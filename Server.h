380ddbae7db0b6f6cfc6140cb41249ea4a0f5fe0
klorin <45018060+klorin@users.noreply.github.com>
2020-01-01 00:27:55 +0200

    Update Server.h

--- Server.h
+++ Server.h
@@ -6,26 +6,49 @@
 #define DATACENTERSMANAGER2_SERVER_H
 
 #include "Data Structures/Set.h"
-
+/*
 class Server { //TODO:INCOMPLETE!
-private: //DEBUGGING NOTE: Make public to be able to run the test currently in main
+public: //DEBUGGING NOTE: Make public to be able to run the test currently in main
     unsigned int traffic = 0;
-public:
-    void setTraffic(unsigned int traffic);
-
-private:
     Set<Server>::SetNode link;
+};
+*/
 
+class Server { //Roi's version
+protected: //DEBUGGING NOTE: Make public to be able to run the test currently in main
+    int id;
+    int traffic;
 public:
-    bool operator>(const Server &other);
+    explicit Server(int given_id = 0, int given_traffic = 0): id(given_id), traffic(given_traffic) {}
+    //destructor
+    virtual ~Server()= default;
+    void setTraffic(int given_traffic) { traffic = given_traffic;}
+    int getTraffic() const {return traffic;}
+    int getID() const {return id;}
+    //operators
+    bool operator==(const Server &server) const {
+        return (server.id == id);
+    }
+    virtual bool operator> (const Server &server) const {
+        if (traffic > server.traffic) return true;
+        if (traffic < server.traffic) return false;
+        if (traffic == server.traffic){
+            return id > server.id;
+        }
+        return false;
+    }
 };
 
-bool Server::operator>(const Server &other) {
-    return traffic > other.traffic;
-}
 
-void Server::setTraffic(unsigned int traffic) {
-    Server::traffic = traffic;
-}
+class ServerAux: public Server {
+public:
+    ~ServerAux() override = default;
+    bool operator> (const Server &server) const override {
+        return id>server.getID();
+    }
+
+public:
+    explicit ServerAux(int given_id = 0, int given_traffic = 0): Server(given_id,given_traffic){};
+};
 
 #endif //DATACENTERSMANAGER2_SERVER_H
