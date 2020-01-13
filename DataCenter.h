8c37f77b97e0462bb999636ff7679d24d7efe597
aa bb <a.block@campus.technion.ac.il>
2020-01-13 20:20:44 +0200

    Fixed multiple definition error

--- DataCenter.h
+++ DataCenter.h
@@ -7,30 +7,35 @@
 #include "Data Structures/AVLTree/AVLTree.h"
 #include "Server.h"
 
-//ROI: please review merge function to make sure it updates everything that needs to be updated when DataCenters are united.
-// Note that calling merge modifies the original DataCenter!  (Instead of returning a new one)
 
-
-class DataCenter { //Roi's version
+class DataCenter {
 public:
-    DataCenter &merge(DataCenter &other);  //AKIVA: made this public
+    DataCenter &merge(DataCenter &other) {
+        servers_num += other.servers_num;
+        servers_tree = servers_tree.merge(other.servers_tree);
+        return *this;
+    }
 
     int id;
 
-    void setID(int id); //AKIVA
+    void setID(int id) {
+        DataCenter::id = id;
+    }
 
     int servers_num;
     AVLTree<Server> servers_tree;
-    enum DataCenterError{SUCCESS = 0, FAILURE = -1, ALLOCATION_ERROR = -2, INVALID_INPUT = -3};
+    enum DataCenterError {
+        SUCCESS = 0, FAILURE = -1, ALLOCATION_ERROR = -2, INVALID_INPUT = -3
+    };
 public:
     //ctor
     explicit DataCenter(int given_id = 0) : id(given_id), servers_num(0) {}
 
-    void IncrementSize() {servers_num++;}
+    void IncrementSize() { servers_num++; }
 
     void DecrementSize() { servers_num--; }//AKIVA
 
-    DataCenterError AddServer(int given_id){
+    DataCenterError AddServer(int given_id) {
         //check in outer layers that the id is correct
         //check in outer layer that id isn;t already inside HashTable
         Server temp_server(given_id);
@@ -38,12 +43,12 @@ public:
             servers_tree.insert(temp_server);
             return SUCCESS;
         }
-        catch (DataManagerExceptions::Exceptions &e){
+        catch (DataManagerExceptions::Exceptions &e) {
             return FAILURE;
         }
     }
 
-    DataCenterError RemoveServer(int ServerID){
+    DataCenterError RemoveServer(int ServerID) {
         //check in outer layers that the id is correct
         Server temp_server(ServerID);
         try {
@@ -51,22 +56,22 @@ public:
             servers_num--;
             return SUCCESS;
         }
-        catch (DataManagerExceptions::Exceptions &e){
+        catch (DataManagerExceptions::Exceptions &e) {
             return FAILURE;
         }
     }
 
-    DataCenterError SetTraffic(int ServerID, int new_traffic, int old_traffic){
+    DataCenterError SetTraffic(int ServerID, int new_traffic, int old_traffic) {
         //check in traffic and server number are legal
         //check server in hash table
         Server temp_server(ServerID, old_traffic);
-        Server &server_target =servers_tree.find(temp_server);
+        Server &server_target = servers_tree.find(temp_server);
         server_target.setTraffic(new_traffic);
         //servers_tree.insert(temp_server);
     }
 
-    DataCenterError SumHighestTrafficServers(int k, int *traffic){
-     //check k> 0
+    DataCenterError SumHighestTrafficServers(int k, int *traffic) {
+        //check k> 0
         if (k > servers_num) return FAILURE;
         int tree_size = servers_tree.getTreeSize();
         Server max_server = servers_tree[tree_size];
@@ -77,21 +82,12 @@ public:
             k = tree_size - k;
             Server kth_server = servers_tree[k];
             *traffic = servers_tree.getPartialSum(max_server) - servers_tree.getPartialSum(kth_server);
-         return SUCCESS;
-     }
+            return SUCCESS;
+        }
 
 
     };
 
-DataCenter &DataCenter::merge(DataCenter &other) {
-    servers_num += other.servers_num;
-    servers_tree = servers_tree.merge(other.servers_tree);
-    //TODO: update anything else in DataCenter that needs to be updated in a merge
-    return *this;
-}
-
-void DataCenter::setID(int id) {
-    DataCenter::id = id;
-}
+};
 
 #endif //DATACENTERSMANAGER2_DATACENTER_H
