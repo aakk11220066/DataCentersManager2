12e98a0634d7700015eb65d763030443d8fc65fa
klorin <klorin.roi@cqmpus.technion.ac.il>
2020-01-13 20:06:01 +0200

    almost done testing DataCenter

--- main.cpp
+++ main.cpp
@@ -88,9 +88,9 @@ int main() {
     DataCenter dc1(1);
     DataCenter dc2(2);
     dc1.AddServer(6);
-    dc1.SetTraffic(6,6);
+    dc1.SetTraffic(6,6, 0);
     dc1.AddServer(7);
-    dc1.SetTraffic(7,7);
+    dc1.SetTraffic(7,7, 0);
     dc2.AddServer(8);
     //dc2.SetTraffic(8,8);
     //------DataCenterManager initialization test---------
